368567d0ae8603e615d7b85d0b4df328fde34dde
userexistserror <userexistserror@gmail.com>
2019-11-27 15:38:27 -0800

    Bug fixes: Edit/Add Signature dialog is now properly centered. JLabels and JTextFields are now better aligned in profile editor dialog. New: optional profile fields now contain hint text that specify they are optional

--- src/burp/AWSProfileEditorDialog.java
+++ src/burp/AWSProfileEditorDialog.java
@@ -5,6 +5,8 @@ import javax.swing.border.TitledBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 
 public class AWSProfileEditorDialog extends JDialog
 {
@@ -61,38 +63,38 @@ public class AWSProfileEditorDialog extends JDialog
         // panel for required fields
         JPanel basicPanel = new JPanel(new GridBagLayout());
         basicPanel.setBorder(new TitledBorder("Credentials"));
-        basicPanel.add(new JLabel("Name"), newConstraint(0, 0, GridBagConstraints.FIRST_LINE_START));
+        basicPanel.add(new JLabel("Name"), newConstraint(0, 0, GridBagConstraints.LINE_START));
         this.nameTextField = new JTextField("", TEXT_FIELD_WIDTH);
         basicPanel.add(nameTextField, newConstraint(1, 0));
-        basicPanel.add(new JLabel("KeyId"), newConstraint(0, 1, GridBagConstraints.FIRST_LINE_START));
+        basicPanel.add(new JLabel("KeyId"), newConstraint(0, 1, GridBagConstraints.LINE_START));
         this.keyIdTextField = new JTextField("", TEXT_FIELD_WIDTH);
         basicPanel.add(keyIdTextField, newConstraint(1, 1));
-        basicPanel.add(new JLabel("SecretKey"), newConstraint(0, 2, GridBagConstraints.FIRST_LINE_START));
+        basicPanel.add(new JLabel("SecretKey"), newConstraint(0, 2, GridBagConstraints.LINE_START));
         this.secretKeyTextField = new JTextField("", TEXT_FIELD_WIDTH);
         basicPanel.add(secretKeyTextField, newConstraint(1, 2));
-        basicPanel.add(new JLabel("Region"), newConstraint(0, 3, GridBagConstraints.FIRST_LINE_START));
-        this.regionTextField = new JTextField("", TEXT_FIELD_WIDTH);
+        basicPanel.add(new JLabel("Region"), newConstraint(0, 3, GridBagConstraints.LINE_START));
+        this.regionTextField = new OptionalJTextField("", TEXT_FIELD_WIDTH);
         basicPanel.add(regionTextField, newConstraint(1, 3));
-        basicPanel.add(new JLabel("Service"), newConstraint(0, 4, GridBagConstraints.FIRST_LINE_START));
-        this.serviceTextField = new JTextField("", TEXT_FIELD_WIDTH);
+        basicPanel.add(new JLabel("Service"), newConstraint(0, 4, GridBagConstraints.LINE_START));
+        this.serviceTextField = new OptionalJTextField("", TEXT_FIELD_WIDTH);
         basicPanel.add(serviceTextField, newConstraint(1, 4));
-        outerPanel.add(basicPanel, newConstraint(0, 0, GridBagConstraints.FIRST_LINE_START));
+        outerPanel.add(basicPanel, newConstraint(0, 0, GridBagConstraints.LINE_START));
 
         // panel for assume role fields
         this.assumeRoleCheckbox = new JCheckBox("Assume a role", false);
-        outerPanel.add(assumeRoleCheckbox, newConstraint(0, 1, GridBagConstraints.FIRST_LINE_START));
+        outerPanel.add(assumeRoleCheckbox, newConstraint(0, 1, GridBagConstraints.LINE_START));
         JPanel rolePanel = new JPanel(new GridBagLayout());
         rolePanel.setBorder(new TitledBorder("Role"));
-        rolePanel.add(new JLabel("RoleArn"), newConstraint(0, 0, GridBagConstraints.FIRST_LINE_START));
+        rolePanel.add(new JLabel("RoleArn"), newConstraint(0, 0, GridBagConstraints.LINE_START));
         this.roleArnTextField = new JTextField("", TEXT_FIELD_WIDTH-2);
         rolePanel.add(this.roleArnTextField, newConstraint(1, 0));
-        rolePanel.add(new JLabel("SessionName"), newConstraint(0, 2, GridBagConstraints.FIRST_LINE_START));
-        this.sessionNameTextField = new JTextField("", TEXT_FIELD_WIDTH-2);
+        rolePanel.add(new JLabel("SessionName"), newConstraint(0, 2, GridBagConstraints.LINE_START));
+        this.sessionNameTextField = new OptionalJTextField("", TEXT_FIELD_WIDTH-2);
         rolePanel.add(this.sessionNameTextField, newConstraint(1, 2));
-        rolePanel.add(new JLabel("ExternalId"), newConstraint(0, 1, GridBagConstraints.FIRST_LINE_START));
-        this.externalIdTextField = new JTextField("", TEXT_FIELD_WIDTH-2);
+        rolePanel.add(new JLabel("ExternalId"), newConstraint(0, 1, GridBagConstraints.LINE_START));
+        this.externalIdTextField = new OptionalJTextField("", TEXT_FIELD_WIDTH-2);
         rolePanel.add(this.externalIdTextField, newConstraint(1, 1));
-        outerPanel.add(rolePanel, newConstraint(0, 2, GridBagConstraints.FIRST_LINE_START));
+        outerPanel.add(rolePanel, newConstraint(0, 2, GridBagConstraints.LINE_START));
 
         statusLabel = new JLabel("<html><i>Ok to submit</i></html>");
         statusLabel.setForeground(burp.textOrange);
@@ -169,7 +171,8 @@ public class AWSProfileEditorDialog extends JDialog
 
         add(outerPanel);
         pack();
-        setLocationRelativeTo(burp.getUiComponent());
+        // setting to burp.getUiComponent() is not sufficient for dialogs popped outside the SigV4 tab.
+        setLocationRelativeTo(SwingUtilities.getWindowAncestor(burp.getUiComponent()));
     }
 
     protected void setStatusLabel(final String message)
@@ -194,6 +197,79 @@ public class AWSProfileEditorDialog extends JDialog
                 sessionNameTextField.setText(profile.getAssumeRole().getSessionName());
                 externalIdTextField.setText(profile.getAssumeRole().getExternalId());
             }
+        }
+    }
+}
+
+
+/*
+This class implements a JTextField with "Optional" hint text when no user input is present.
+ */
+class OptionalJTextField extends JTextField implements FocusListener
+{
+    private Font defaultFont;
+    private Color defaultForegroundColor;
+    private Color optionalForegroundColor;
+
+    public OptionalJTextField(String content, int width) {
+        super(content, width);
+        init();
+    }
+
+    public OptionalJTextField(String content) {
+        super(content);
+        init();
+    }
+
+    void init() {
+        defaultFont = getFont();
+        addFocusListener(this);
+        defaultForegroundColor = getForeground();
+        optionalForegroundColor = new Color(0xbb, 0xbb, 0xbb);
+        if (super.getText().equals("")) {
+            setHintText();
+        }
+    }
+
+    @Override
+    public String getText() {
+        // make sure we don't return "Optional" when these fields are saved
+        if (getFont().isItalic()) {
+            return "";
+        }
+        return super.getText();
+    }
+
+    @Override
+    public void setText(final String text) {
+        if (!text.equals("")) {
+            setUserText(text);
+        }
+    }
+
+    private void setHintText() {
+        setFont(new Font(defaultFont.getFamily(), Font.ITALIC, defaultFont.getSize()));
+        setForeground(optionalForegroundColor);
+        super.setText("Optional");
+    }
+
+    private void setUserText(final String text) {
+        setFont(defaultFont);
+        setForeground(defaultForegroundColor);
+        super.setText(text);
+    }
+
+    @Override
+    public void focusGained(FocusEvent focusEvent) {
+        if (getFont().isItalic()) {
+            setUserText("");
+        }
+    }
+
+    @Override
+    public void focusLost(FocusEvent focusEvent) {
+        if (super.getText().equals("")) {
+            setHintText();
         }
     }
 }
