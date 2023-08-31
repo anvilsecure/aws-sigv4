0bdcd4312c8d6cbfe1fdaadc947a63474db31c4d
UserExistsError <userexistserror@gmail.com>
2023-08-31 15:56:49 -0700

    enable double-click to edit on profile table

--- src/main/java/burp/BurpExtender.java
+++ src/main/java/burp/BurpExtender.java
@@ -27,6 +27,8 @@ import java.awt.datatransfer.Clipboard;
 import java.awt.datatransfer.StringSelection;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.net.MalformedURLException;
@@ -193,6 +195,14 @@ public class BurpExtender implements IBurpExtender, IHttpListener, ITab, IExtens
             {
                 // prevent table cells from being edited. must use dialog to edit.
                 return false;
+            }
+        });
+        profileTable.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                if (e.getClickCount() == 2) {
+                    editProfileButton.doClick();
+                }
             }
         });
 
